ff054ccfa5f34a10e6f359d250921eb25ae9a904
Nikolas Laus <49353043+blinchk@users.noreply.github.com>
2020-11-03 02:01:13 +0200

    Change JSON url in script.js to github json link

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function getQuote() {
-    $.getJSON("..//quotes.json",
+    $.getJSON("https://raw.githubusercontent.com/blinchk/kitchen-max-quotes/master/quotes.json",
     function (data, textStatus, jqXHR) {
         console.log(data[Math.random() * data.length+1]);
         var quote_text = data[Math.floor(Math.random() * data.length)];
